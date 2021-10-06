a146b68fa02310096e59924e38108140b919e5da
carlos23fb <jcflobe@gmail.com>
2021-10-06 13:52:36 -0600

    setup Vuex Store

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from "vuex";
+
+import cart from './modules/cart'
+import product from './modules/product'
+
+
+export default createStore({
+    modules:{
+        cart,
+        product
+    }
+})
